14c6e2abd53da2c392ba5b2eebd220aa879da849
tushar1210 <ts26192271@gmail.com>
2019-05-04 12:45:29 +0530

    Env set

---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'AngelHack-19' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for AngelHack-19
+
+pod 'Firebase/Core'
+pod 'Firebase/Database'
+pod 'SwiftyJSON'
+pod 'MaterialComponents/BottomAppBar+ColorThemer'
+
+end
---
+++ Pods/Target Support Files/MaterialComponents/MaterialComponents-umbrella.h
@@ -0,0 +1,72 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "MaterialBottomAppBar.h"
+#import "MDCBottomAppBarView.h"
+#import "MaterialBottomAppBar+ColorThemer.h"
+#import "MDCBottomAppBarColorThemer.h"
+#import "MaterialButtonBar.h"
+#import "MDCButtonBar.h"
+#import "MDCButtonBarButton.h"
+#import "MaterialButtons.h"
+#import "MDCButton.h"
+#import "MDCFlatButton.h"
+#import "MDCFloatingButton+Animation.h"
+#import "MDCFloatingButton.h"
+#import "MDCRaisedButton.h"
+#import "MaterialInk.h"
+#import "MDCInkGestureRecognizer.h"
+#import "MDCInkTouchController.h"
+#import "MDCInkView.h"
+#import "MaterialNavigationBar.h"
+#import "MDCNavigationBar.h"
+#import "MaterialShadowElevations.h"
+#import "MDCShadowElevations.h"
+#import "MaterialShadowLayer.h"
+#import "MDCShadowLayer.h"
+#import "MaterialShapes.h"
+#import "MDCCornerTreatment.h"
+#import "MDCEdgeTreatment.h"
+#import "MDCPathGenerator.h"
+#import "MDCRectangleShapeGenerator.h"
+#import "MDCShapedShadowLayer.h"
+#import "MDCShapedView.h"
+#import "MDCShapeGenerating.h"
+#import "MaterialThemes.h"
+#import "MDCColorScheme.h"
+#import "MDCFontScheme.h"
+#import "MDCTonalColorScheme.h"
+#import "MDCTonalPalette.h"
+#import "MaterialTypography.h"
+#import "MDCFontScaler.h"
+#import "MDCFontTextStyle.h"
+#import "MDCTypography.h"
+#import "UIFont+MaterialScalable.h"
+#import "UIFont+MaterialSimpleEquality.h"
+#import "UIFont+MaterialTypography.h"
+#import "UIFontDescriptor+MaterialTypography.h"
+#import "MaterialApplication.h"
+#import "UIApplication+AppExtensions.h"
+#import "MaterialMath.h"
+#import "MDCMath.h"
+#import "MaterialColorScheme.h"
+#import "MDCLegacyColorScheme.h"
+#import "MDCLegacyTonalColorScheme.h"
+#import "MDCLegacyTonalPalette.h"
+#import "MDCSemanticColorScheme.h"
+#import "MaterialTypographyScheme.h"
+#import "MDCLegacyFontScheme.h"
+#import "MDCTypographyScheme.h"
+
+FOUNDATION_EXPORT double MaterialComponentsVersionNumber;
+FOUNDATION_EXPORT const unsigned char MaterialComponentsVersionString[];
+
